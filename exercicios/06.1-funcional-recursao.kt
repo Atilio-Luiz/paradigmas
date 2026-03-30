776b169cb771a020c6d0e01d6d8be2f6fe47e307
Atilio Luiz <gomes.atilio@gmail.com>
2026-03-30 09:03:43 -0300

    functional update

--- exercicios/06.1-funcional-recursao.kt
+++ exercicios/06.1-funcional-recursao.kt
@@ -11,41 +11,41 @@ fun main() {
     /**
      * Dados dois valores inteiros A e B recebidos como parâmetro, retorne a soma de todos os valores de A até B (incluindo A e B).
      * Se A for maior que B, retorne 0.
-     * Versão usando comandos de repetição.
+     * Versão usando recursão de cauda.
      */
-    fun somaIntervaloRepeticao(a: Int, b: Int): Int {
+    fun somaIntervaloRecursivo(a: Int, b: Int): Int {
         // <FORNEÇA AQUI SUA SOLUÇÃO>
         return 0
     }
 
     // <INCLUA O TRECHO ABAIXO PARA TESTAR SUA SOLUÇÃO>
     /*
-    assertEquals("somaIntervaloRepeticao(1, 10)", 55, somaIntervaloRepeticao(1, 10))
-    assertEquals("somaIntervaloRepeticao(10, 20)", 165, somaIntervaloRepeticao(10, 20))
-    assertEquals("somaIntervaloRepeticao(1, 100)", 5050, somaIntervaloRepeticao(1, 100))
-    assertEquals("somaIntervaloRepeticao(10, 5)", 0, somaIntervaloRepeticao(10, 5))
-    assertEquals("somaIntervaloRepeticao(10, 10)", 10, somaIntervaloRepeticao(10, 10))
-    println("Todos os testes passaram para a função somaIntervaloRepeticao!")
+    assertEquals("somaIntervaloRecursivo(1, 10)", 55, somaIntervaloRecursivo(1, 10))
+    assertEquals("somaIntervaloRecursivo(10, 20)", 165, somaIntervaloRecursivo(10, 20))
+    assertEquals("somaIntervaloRecursivo(1, 100)", 5050, somaIntervaloRecursivo(1, 100))
+    assertEquals("somaIntervaloRecursivo(10, 5)", 0, somaIntervaloRecursivo(10, 5))
+    assertEquals("somaIntervaloRecursivo(10, 10)", 10, somaIntervaloRecursivo(10, 10))
+    println("Todos os testes passaram para a função somaIntervaloRecursivo!")
     */
 
     /**
      * Dados dois valores inteiros A e B recebidos como parâmetro, retorne a soma de todos os valores pares de A até B (possivelmente incluindo A e B).
      * Se A for maior que B, retorne 0.
-     * Versão usando comandos de repetição.
+     * Versão usando recursão de cauda.
      */
-    fun somaParesIntervaloRepeticao(a: Int, b: Int): Int {
+    fun somaParesIntervaloRecursivo(a: Int, b: Int): Int {
         // <FORNEÇA AQUI SUA SOLUÇÃO>
         return 0
     }
 
     // <INCLUA O TRECHO ABAIXO PARA TESTAR SUA SOLUÇÃO>
     /*
-    assertEquals("somaParesIntervaloRepeticao(1, 10)", 30, somaParesIntervaloRepeticao(1, 10))
-    assertEquals("somaParesIntervaloRepeticao(10, 20)", 90, somaParesIntervaloRepeticao(10, 20))
-    assertEquals("somaParesIntervaloRepeticao(1, 100)", 2550, somaParesIntervaloRepeticao(1, 100))
-    assertEquals("somaParesIntervaloRepeticao(10, 5)", 0, somaParesIntervaloRepeticao(10, 5))
-    assertEquals("somaParesIntervaloRepeticao(10, 10)", 10, somaParesIntervaloRepeticao(10, 10))
-    println("Todos os testes passaram para a função somaParesIntervaloRepeticao!")
+    assertEquals("somaParesIntervaloRecursivo(1, 10)", 30, somaParesIntervaloRecursivo(1, 10))
+    assertEquals("somaParesIntervaloRecursivo(10, 20)", 90, somaParesIntervaloRecursivo(10, 20))
+    assertEquals("somaParesIntervaloRecursivo(1, 100)", 2550, somaParesIntervaloRecursivo(1, 100))
+    assertEquals("somaParesIntervaloRecursivo(10, 5)", 0, somaParesIntervaloRecursivo(10, 5))
+    assertEquals("somaParesIntervaloRecursivo(10, 10)", 10, somaParesIntervaloRecursivo(10, 10))
+    println("Todos os testes passaram para a função somaParesIntervaloRecursivo!")
     */
 
     /*
@@ -53,7 +53,7 @@ fun main() {
      * carta Ás descartada. Dado o total de pontos das cartas de um jogador e a quantidade de cartas
      * Ás que ele dispõe, retorne o total de pontos mais próximo possível de 21 pontos que pode ser 
      * obtido, sem ultrapassar 21 pontos (se possível).
-     * Versão usando comandos de repetição.
+     * Versão usando recursão de cauda.
      */
     fun blackjack(pontos: Int, ases: Int): Int {
         // <FORNEÇA AQUI SUA SOLUÇÃO>
@@ -78,7 +78,7 @@ fun main() {
      * Sequência de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
      * O termo 0 é o primeiro termo da sequência.
      * O n-ésimo termo é a soma dos dois termos anteriores.
-     * Versão usando comandos de repetição.
+     * Versão usando recursão de cauda.
      */
     fun fibonacci(n: Int): Int {
         // <FORNEÇA AQUI SUA SOLUÇÃO>
